082565690f6819a109eea39e80de08c9b5fc62ba
Daniel Cruz Santos <canuardaniel@gmail.com>
2021-10-12 21:56:29 -0500

    Fin seccion 18

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcryptjs');
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/jwt");
 const { googleVerify } = require("../helpers/google-verify");
+const { getMenuFrontEnd } = require("../helpers/menu-frontend");
 
 
 const login = async ( req, res= response ) => {
@@ -40,7 +41,8 @@ const login = async ( req, res= response ) => {
 
         res.json({
             ok: true,
-            token
+            token,
+            menu: getMenuFrontEnd( usuarioDB.role )
         });
 
     } catch (error) {
@@ -89,7 +91,8 @@ const googleSignIn = async ( req, res = response) => {
 
         res.json({
             ok: true,
-            token
+            token,
+            menu: getMenuFrontEnd( usuario.role )
         });
         
     } catch (error) {
@@ -116,7 +119,8 @@ const renewToken = async( req, res= response) => {
     res.json({
         ok: true,
         token,
-        usuario
+        usuario,
+        menu: getMenuFrontEnd( usuario.role )
     });
 }
 
